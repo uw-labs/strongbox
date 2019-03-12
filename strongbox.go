061cb8cb946692f9a60a2d335e3bad0aebea66fa
George Angel <github-7574494a@infosecproject.org>
2019-03-12 18:24:44 +1000

    fix circle builds
    - new regex
    - go v1.12
    - golint

--- strongbox.go
+++ strongbox.go
@@ -479,7 +479,7 @@ func (kr *fileKeyRing) Save() error {
 	if os.IsNotExist(err) {
 		err := os.MkdirAll(path, 0700)
 		if err != nil {
-			return fmt.Errorf("Error creating strongbox home folder: %s\n", err)
+			return fmt.Errorf("error creating strongbox home folder: %s", err)
 		}
 	}
 
